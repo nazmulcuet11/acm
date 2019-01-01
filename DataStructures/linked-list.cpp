874abc89a7de4d46d8e6ca7b91dabf66f762f2f3
nazmulcuet11 <nazmulcuet11@gmail.com>
2019-01-01 14:05:47 +0600

    completes linked list addition deletion

--- DataStructures/linked-list.cpp
+++ DataStructures/linked-list.cpp
@@ -1,35 +1,81 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
-struct List
+struct Node
 {
-private:
-    struct Node
-    {
-        int data;
-        Node *next, *prev;
-        Node(int data) 
-        {
-            this->data = data;
-            prev = next = NULL;
-        }
-    };
-
-    Node *head, *tail;
-public:
-    List() { head = tail = NULL;}
-    void insert(int data);
+    int data;
+    Node *next;
+    Node(int data = 0, Node *next = NULL) {
+        this->data = data;
+        this->next = next;
+    }
 };
 
-void List::insert(int data)
+void print(Node *head)
+{
+    Node *curr = head;
+    while(curr) 
+    {
+        cout << curr->data << "-->";
+        curr = curr->next;
+    }
+    cout << endl;
+}
+
+Node* insert(int data, Node *head)
+{
+    return new Node(data, head);
+}
+
+Node* remove(int data, Node *head)
 {
     if(head == NULL)
+        return NULL;
+
+    if(head->data == data)
     {
-        head = tail = new Node(data);
-        return;
+        Node *temp = head->next;
+        delete head;
+        return temp;
     }
 
-    tail->next = new Node(data);
-    tail = tail->next;
+    Node *curr = head;
+    while(curr && curr->next)
+    {
+        if(curr->next->data == data)
+        {
+            Node *temp = curr->next;
+            curr->next = curr->next->next;
+            delete temp;
+            break;
+        }
+        curr = curr->next;
+    }
+    return head;
 }
+
+int main(int argc, char const *argv[])
+{
+    Node *head = NULL;
+    head = insert(1, head);
+    head = insert(2, head);
+    head = insert(3, head);
+    head = insert(4, head);
+    head = insert(5, head);
+    head = insert(6, head);
+    print(head);
+
+    head = remove(4, head);
+    print(head);
+
+    head = remove(1, head);
+    print(head);
+
+    head = remove(6, head);
+    print(head);
+
+    return 0;
+}
+
+
