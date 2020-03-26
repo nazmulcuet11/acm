8b0fae92ae020d0bf0bcbf79cdb061c8c28b99b5
nazmulcuet11 <nazmulcuet11@gmail.com>
2020-03-26 15:41:32 +0600

    .

--- InterviewBit/bit-manipulation/single-number-II.py
+++ InterviewBit/bit-manipulation/single-number-II.py
@@ -31,19 +31,16 @@ class Solution:
 
         # Over flows
         if ans & (1 << 31) > 0:
+            # we need to manually revert bits because reverting 
+            # bit using ~ will revert bits beyond 32 as python
+            # int does not has range boundary 32 or 64
+
             for i in range(32):
                 ans ^= 1 << i
+            # Calculate 2's complement
             return -(ans + 1)
         
         return ans
-
-# print(Solution().singleNumber([1, 2, 4, 3, 3, 2, 2, 3, 1, 1]))
-# print(Solution().singleNumber([0, 0, 0, 1]))
-ans = Solution().singleNumber([-2,-2,1,1,-3,1,-3,-3,-4,-2])
-print(ans)
-print("{0:b}".format(ans))
-print("{0:b}".format(-4))
-
 
 def print_bits(number, bit_count):
     for i in range(bit_count-1, -1, -1):
@@ -51,8 +48,15 @@ def print_bits(number, bit_count):
         print(bit, end = '')
     print()
 
-print_bits(-2, 32)
-print_bits(21, 32)
-print_bits(~21, 32)
-print_bits(-21, 32)
-print_bits(~-21, 32)
+
+# print_bits(-2, 32)
+# print_bits(21, 32)
+# print_bits(~21, 32)
+# print_bits(-21, 32)
+# print_bits(~-21, 32)
+
+# print(Solution().singleNumber([1, 2, 4, 3, 3, 2, 2, 3, 1, 1]))
+# print(Solution().singleNumber([0, 0, 0, 1]))
+ans = Solution().singleNumber([-2,-2,1,1,-3,1,-3,-3,-4,-2])
+print(ans)
+print_bits(-(~ans + 1), 100)
