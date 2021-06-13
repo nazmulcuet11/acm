0b5c40974555e4d65a615bfeae0d56a8713b408b
Nazmul Islam <nazmulcuet11@gmail.com>
2021-06-13 08:56:23 +0600

    Add explanation for min-steps-in-infinite-grid

--- InterviewBit/arrays/min-steps-in-infinite-grid.py
+++ InterviewBit/arrays/min-steps-in-infinite-grid.py
@@ -1,3 +1,14 @@
+# lets assume we are going to (x, y) from (0, 0)
+# we want to travel minimum distance so we will
+# try to go diagonally, we can move diagonally
+# upto t = min(x, y) steps, after we reach (t, t)
+# we have to max(x, y) - t steps sidewise. So we
+# have to travel max(x, y) - t + t = max(x, y)
+# distance in total.
+#
+# So we can say to reach (x2, y2) from (x1, y1)
+# we must move max(abs(x2-x1), abs(y2-y1)) steps atleast.
+
 class Solution:
     # @param A : list of integers
     # @param B : list of integers
@@ -7,6 +18,7 @@ class Solution:
         for i in range(1, len(A)):
             sum += max(abs(A[i]-A[i-1]), abs(B[i] - B[i-1]))
         return sum
+
 
 # sum = Solution().coverPoints([0, 1, 1], [0, 1, 2])
 sum = Solution().coverPoints([-7, -13], [1, -5])
