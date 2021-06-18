7b7fb0f3e19876618d8ca0a4c312e566164c6975
Nazmul Islam <nazmulcuet11@gmail.com>
2021-06-18 12:04:13 +0600

    Simplify implementation rearrange-array

--- InterviewBit/maths/rearrange-array.py
+++ InterviewBit/maths/rearrange-array.py
@@ -1,35 +1,34 @@
-import resource, sys
-resource.setrlimit(resource.RLIMIT_STACK, (2**29,-1))
+import resource
+import sys
+
+# resource.setrlimit(resource.RLIMIT_STACK, (2**29,-1))
 sys.setrecursionlimit(10**6)
- 
-def dfs(x, A):
-    if A[x] < 0:
-        return A[x]
- 
-    t = A[x]
-    A[x] = -A[x]
-    next = 0 if t == len(A) else t
-    A[x] = dfs(next, A)
-    return -t
- 
+
+
+def dfs(i, A):
+    N = len(A)
+    if A[i] >= N:
+        return A[i]
+
+    t = A[i] = A[i] + N
+    A[i] = dfs(A[i] % N, A)
+    return t
+
+
 class Solution:
     # @param A : list of integers
-    # Modify the array A which is passed by reference. 
-    # You do not need to return anything in this case. 
+    # Modify the array A which is passed by reference.
+    # You do not need to return anything in this case.
     def arrange(self, A):
         N = len(A)
-        for i in range(N): 
-            if A[i] == 0:
-                A[i] = N
-            
-    
         for i in range(N):
-            if A[i] > 0:
+            if A[i] < N:
                 dfs(i, A)
-    
-        for i in range(N): 
-            A[i] = -A[i]
-            if A[i] == N:
-                A[i] = 0
- 
+
+        for i in range(N):
+            A[i] = A[i] % N
+
         return A
+
+
+print(Solution().arrange([4, 0, 2, 1, 3]))
