fc180cc9f866c6989fc9f48dceb88a88774c2ee8
nazmulcuet11 <nazmulcuet11@gmail.com>
2023-11-27 08:06:25 +0700

    add solution to leetcode 2433

---
+++ acm-kotlin/src/main/kotlin/leetcode/Solution2433.kt
@@ -0,0 +1,11 @@
+package leetcode
+
+class Solution2433 {
+    fun findArray(pref: IntArray): IntArray {
+        return (0..< pref.count())
+            .map { i ->
+                (if (i > 0) pref[i - 1] else 0) xor pref[i]
+            }
+            .toIntArray()
+    }
+}
---
+++ acm-kotlin/src/test/kotlin/leetcode/Solution2433Test.kt
@@ -0,0 +1,23 @@
+package leetcode
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class Solution2433Test {
+    private val sut = Solution2433()
+    @Test
+    fun testCaseOne() {
+        assertArrayEquals(
+            intArrayOf(5, 7, 2, 3, 2),
+            sut.findArray(intArrayOf(5, 2, 0, 3, 1))
+        )
+    }
+
+    @Test
+    fun testCaseTwo() {
+        assertArrayEquals(
+            intArrayOf(13),
+            sut.findArray(intArrayOf(13))
+        )
+    }
+}
