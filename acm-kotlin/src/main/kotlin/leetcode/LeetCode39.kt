e005a379e8a3c36c97f18fd4070bc3aed48df8a4
Nazmul Islam <nazmulcuet11@gmail.com>
2023-12-21 22:03:30 +0700

    Make LeetCode39 more functional

--- acm-kotlin/src/main/kotlin/leetcode/LeetCode39.kt
+++ acm-kotlin/src/main/kotlin/leetcode/LeetCode39.kt
@@ -2,12 +2,11 @@ package leetcode
 
 class LeetCode39 {
     fun combinationSum(candidates: IntArray, target: Int): List<List<Int>> {
-        return combinationSum(candidates, 0, target, listOf())
+        return combinationSum(candidates.toList(), target, listOf())
     }
 
     private fun combinationSum(
-        candidates: IntArray,
-        i: Int,
+        candidates: List<Int>,
         target: Int,
         current: List<Int>
     ): List<List<Int>> {
@@ -15,12 +14,11 @@ class LeetCode39 {
             return listOf(current.toList())
         }
 
-        return (i..<candidates.size)
+        return candidates.indices
             .filter { target - candidates[it] >= 0 }
             .flatMap {
                 combinationSum(
-                    candidates,
-                    it,
+                    candidates.drop(it),
                     target - candidates[it],
                     current + listOf(candidates[it])
                 )
