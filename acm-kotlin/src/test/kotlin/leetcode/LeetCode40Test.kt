6dcf5e4895546e3aacda4276a32df8bf4135ef69
Nazmul Islam <nazmulcuet11@gmail.com>
2023-12-25 12:20:37 +0700

    Add kotlin solution for LeetCode40

---
+++ acm-kotlin/src/test/kotlin/leetcode/LeetCode40Test.kt
@@ -0,0 +1,37 @@
+package leetcode
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class LeetCode40Test {
+    private val sut = LeetCode40()
+    
+    @Test
+    fun testCaseOne() {
+        assertEquals(
+            sorted(
+                listOf(
+                    listOf(1,1,6),
+                    listOf(1,2,5),
+                    listOf(1,7),
+                    listOf(2,6),
+                )
+            ),
+            sorted(sorted(sut.combinationSum2(intArrayOf(10,1,2,7,6,1,5), 8)))
+        )
+    }
+
+    @Test
+    fun testCaseTwo() {
+        assertEquals(
+            sorted(
+                listOf(
+                    listOf(1,2,2),
+                    listOf(5),
+                )
+            ),
+            sorted(sut.combinationSum2(intArrayOf(2,5,2,1,2), 5))
+        )
+    }
+}
+
