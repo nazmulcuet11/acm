0ad3542a2412415cb3dbf7bd1817e36f68091b86
nazmulcuet11 <nazmulcuet11@gmail.com>
2024-03-25 20:26:45 +0700

    add recursive solution for leetcode 55 in golang

--- acm-go/leetcode55/leetcode55.go
+++ acm-go/leetcode55/leetcode55.go
@@ -1,5 +1,7 @@
 package leetcode55
 
+/*
+// iterative
 func canJump(nums []int) bool {
 	cache := make([]bool, len(nums))
 	cache[0] = true
@@ -11,4 +13,32 @@ func canJump(nums []int) bool {
 		}
 	}
 	return cache[len(nums)-1]
+}
+*/
+
+// recursive
+func canJump(nums []int) bool {
+	cache := make([]int, len(nums))
+	for i := 0; i < len(nums); i++ {
+		cache[i] = -1
+	}
+
+	var solve func(int) int
+	solve = func(i int) int {
+		if i >= len(nums)-1 {
+			return 1
+		}
+
+		if cache[i] != -1 {
+			return cache[i]
+		}
+
+		cache[i] = 0
+		for j := i + 1; j <= i+nums[i] && j < len(nums); j++ {
+			cache[i] |= solve(j)
+		}
+		return cache[i]
+	}
+
+	return solve(0) == 1
 }
