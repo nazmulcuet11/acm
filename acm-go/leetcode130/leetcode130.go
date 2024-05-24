ce2be0ce53df906b69262a91e3c854a2aa2ea595
nazmulcuet11 <nazmulcuet11@gmail.com>
2024-05-24 21:04:37 +0700

    update leetcode130/leetcode130.go

--- acm-go/leetcode130/leetcode130.go
+++ acm-go/leetcode130/leetcode130.go
@@ -10,24 +10,6 @@ var moves = []struct {
 	{0, -1},
 }
 
-func isBounded(grid [][]byte, i int, j int) bool {
-	if i < 0 || i >= len(grid) || j < 0 || j >= len(grid[i]) {
-		return false
-	}
-
-	if grid[i][j] != 'O' {
-		return true
-	}
-
-	grid[i][j] = 'V'
-	ans := true
-	for _, move := range moves {
-		next := isBounded(grid, i+move.x, j+move.y)
-		ans = ans && next
-	}
-	return ans
-}
-
 func mark(grid [][]byte, i int, j int, current byte, target byte) {
 	if i < 0 || i >= len(grid) || j < 0 || j >= len(grid[i]) || grid[i][j] != current {
 		return
@@ -39,38 +21,43 @@ func mark(grid [][]byte, i int, j int, current byte, target byte) {
 	}
 }
 
-// func printBoard(board [][]byte) {
-// 	fmt.Println("Board")
-// 	fmt.Println("-------------")
-// 	for _, row := range board {
-// 		fmt.Println(string(row))
-// 	}
-// 	fmt.Println("-------------")
-// }
-
 func solve(board [][]byte) {
-	for i := 0; i < len(board); i++ {
-		for j := 0; j < len(board[i]); j++ {
+	m := len(board)
+	if m == 0 {
+		return
+	}
+	n := len(board[0])
+
+	for i := 0; i < m; i++ {
+		if board[i][0] == 'O' {
+			mark(board, i, 0, 'O', 'U')
+		}
+		if board[i][n-1] == 'O' {
+			mark(board, i, n-1, 'O', 'U')
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		if board[0][i] == 'O' {
+			mark(board, 0, i, 'O', 'U')
+		}
+		if board[m-1][i] == 'O' {
+			mark(board, m-1, i, 'O', 'U')
+		}
+	}
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
 			if board[i][j] == 'O' {
-				if isBounded(board, i, j) {
-					// printBoard(board)
-					mark(board, i, j, 'V', 'B')
-					// printBoard(board)
-				} else {
-					// printBoard(board)
-					mark(board, i, j, 'V', 'U')
-					// printBoard(board)
-				}
+				board[i][j] = 'X'
 			}
 		}
 	}
 
-	for i := 0; i < len(board); i++ {
-		for j := 0; j < len(board[i]); j++ {
-			if board[i][j] == 'B' {
-				mark(board, i, j, 'B', 'X')
-			} else if board[i][j] == 'U' {
-				mark(board, i, j, 'U', 'O')
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if board[i][j] == 'U' {
+				board[i][j] = 'O'
 			}
 		}
 	}
