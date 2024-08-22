a51a1a9ffa539f96bda0345a12bdaa5f853a8307
nazmulcuet11 <nazmulcuet11@gmail.com>
2024-08-22 07:27:19 +0700

    add LeetCode52 solution

---
+++ acm-kotlin/src/main/kotlin/leetcode/LeetCode52.kt
@@ -0,0 +1,51 @@
+package leetcode
+
+import kotlin.math.abs
+
+class LeetCode52 {
+    data class Queen(
+        val x: Int,
+        val y: Int
+    )
+
+    private var n: Int = 0
+    private var queens = mutableListOf<Queen>()
+
+    private fun canPlace(newQueen: Queen): Boolean {
+        for (queen in queens) {
+            if (queen.x == newQueen.x) {
+                return false
+            }
+            if (queen.y == newQueen.y) {
+                return false
+            }
+            if (abs(queen.x - newQueen.x) == abs(queen.y - newQueen.y)) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun solve(row: Int): Int {
+        if (queens.size == n) {
+            return 1
+        }
+
+        var ans = 0
+        for (col in 0..<n) {
+            val newQueen = Queen(row, col)
+            if (canPlace(newQueen)) {
+                queens.add(newQueen)
+                ans += solve(row + 1)
+                queens.removeLast()
+            }
+        }
+        return ans
+    }
+
+    fun totalNQueens(n: Int): Int {
+        this.n = n
+        this.queens = mutableListOf()
+        return solve(0)
+    }
+}
