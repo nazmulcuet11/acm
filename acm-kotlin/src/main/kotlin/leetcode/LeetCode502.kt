bce87b2a69f63f8ef8897f2b84aebf1d9ca6c365
nazmulcuet11 <nazmulcuet11@gmail.com>
2024-08-24 08:14:35 +0700

    update LeetCode502 solution using builtin priority queue

--- acm-kotlin/src/main/kotlin/leetcode/LeetCode502.kt
+++ acm-kotlin/src/main/kotlin/leetcode/LeetCode502.kt
@@ -1,70 +1,72 @@
 package leetcode
 
-fun <E> MutableList<E>.swap(i: Int, j: Int) {
-    val t = this[i]
-    this[i] = this[j]
-    this[j] = t
-}
-
-class Heap<E: Comparable<E>> {
-    private var elements = mutableListOf<E>()
-
-    val size: Int
-        get() = elements.size
-
-    val isEmpty: Boolean
-        get() = size == 0
-
-    fun top(): E? = elements.firstOrNull()
-
-    fun push(e: E) {
-        elements.add(e)
-        swim(size - 1)
-    }
-
-    fun pop(): E? {
-        if (isEmpty) {
-            return null
-        }
-
-        elements.swap(0, size - 1)
-        val last = elements.removeLast()
-        if (isEmpty.not()) {
-            dive(0)
-        }
-        return last
-    }
-
-    private fun dive(i: Int) {
-        var m = i
-        val c1 = i * 2 + 1
-        val c2 = i * 2 + 2
-        if (c1 < size && elements[c1] > elements[m]) {
-            m = c1
-        }
-
-        if (c2 < size && elements[c2] > elements[m]) {
-            m = c2
-        }
-
-        if (m != i) {
-            elements.swap(i, m)
-            dive(m)
-        }
-    }
-
-    private fun swim(i: Int) {
-        if (i == 0) {
-            return
-        }
-
-        val p = (i - 1) / 2
-        if (elements[p] < elements[i]) {
-            elements.swap(i, p)
-            swim(p)
-        }
-    }
-}
+import java.util.*
+//
+//fun <E> MutableList<E>.swap(i: Int, j: Int) {
+//    val t = this[i]
+//    this[i] = this[j]
+//    this[j] = t
+//}
+//
+//class Heap<E: Comparable<E>> {
+//    private var elements = mutableListOf<E>()
+//
+//    val size: Int
+//        get() = elements.size
+//
+//    val isEmpty: Boolean
+//        get() = size == 0
+//
+//    fun top(): E? = elements.firstOrNull()
+//
+//    fun push(e: E) {
+//        elements.add(e)
+//        swim(size - 1)
+//    }
+//
+//    fun pop(): E? {
+//        if (isEmpty) {
+//            return null
+//        }
+//
+//        elements.swap(0, size - 1)
+//        val last = elements.removeLast()
+//        if (isEmpty.not()) {
+//            dive(0)
+//        }
+//        return last
+//    }
+//
+//    private fun dive(i: Int) {
+//        var m = i
+//        val c1 = i * 2 + 1
+//        val c2 = i * 2 + 2
+//        if (c1 < size && elements[c1] > elements[m]) {
+//            m = c1
+//        }
+//
+//        if (c2 < size && elements[c2] > elements[m]) {
+//            m = c2
+//        }
+//
+//        if (m != i) {
+//            elements.swap(i, m)
+//            dive(m)
+//        }
+//    }
+//
+//    private fun swim(i: Int) {
+//        if (i == 0) {
+//            return
+//        }
+//
+//        val p = (i - 1) / 2
+//        if (elements[p] < elements[i]) {
+//            elements.swap(i, p)
+//            swim(p)
+//        }
+//    }
+//}
 
 data class Project (
     val capital: Int,
@@ -72,9 +74,9 @@ data class Project (
 ): Comparable<Project> {
     override fun compareTo(other: Project): Int {
         return if (this.profit < other.profit) {
-            -1
-        } else if (this.profit > other.profit) {
             1
+        } else if (this.profit > other.profit) {
+            -1
         } else {
             0
         }
@@ -92,17 +94,17 @@ class LeetCode502 {
 
         projects.sortBy { it.capital }
 
-        val heap = Heap<Project>()
+        val heap = PriorityQueue<Project>()
         var totalCapital = w
         var i = 0
         for (x in 0..<k) {
             while (i < projects.size && projects[i].capital <= totalCapital) {
-                heap.push(projects[i])
+                heap.add(projects[i])
                 i++
             }
 
-            heap.pop()?.let {
-                totalCapital += it.profit
+            if (heap.isNotEmpty()) {
+                totalCapital += heap.poll().profit
             }
         }
         return totalCapital
