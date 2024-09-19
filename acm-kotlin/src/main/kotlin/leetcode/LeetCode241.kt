cec2aff0c158d3dcc9ea418a4d3b014bf8288556
nazmulcuet11 <nazmulcuet11@gmail.com>
2024-09-19 10:55:53 +0700

    add LeetCode241 solution

---
+++ acm-kotlin/src/main/kotlin/leetcode/LeetCode241.kt
@@ -0,0 +1,47 @@
+package leetcode
+
+class LeetCode241 {
+    private fun String.isNumber(): Boolean {
+        for (c in this) {
+            if (!c.isDigit()) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun compute(a: Int, b: Int, op: Char): Int {
+        return when (op) {
+            '+' -> a + b
+            '-' -> a - b
+            '*' -> a * b
+            else -> error("invalid operation")
+        }
+    }
+
+    private fun combine(l: List<Int>, r: List<Int>, op: Char): List<Int> {
+        val combined = mutableListOf<Int>()
+        for (a in l) {
+            for (b in r) {
+                combined.add(compute(a, b, op))
+            }
+        }
+        return combined
+    }
+
+    fun diffWaysToCompute(expression: String): List<Int> {
+        if (expression.isNumber()) {
+            return listOf(expression.toInt())
+        }
+
+        val ans = mutableListOf<Int>()
+        for ((i, c) in expression.withIndex()) {
+            if (!c.isDigit()) {
+                val l = diffWaysToCompute(expression.substring(0, i))
+                val r = diffWaysToCompute(expression.substring(i + 1, expression.length))
+                ans += combine(l, r, c)
+            }
+        }
+        return ans
+    }
+}
