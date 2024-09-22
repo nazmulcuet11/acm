2a9c61cfae193569fb360c5075a6496e237716a9
nazmulcuet11 <nazmulcuet11@gmail.com>
2024-09-22 13:41:02 +0700

    add LeetCode386 alternate solution

--- acm-kotlin/src/main/kotlin/leetcode/LeetCode386.kt
+++ acm-kotlin/src/main/kotlin/leetcode/LeetCode386.kt
@@ -1,5 +1,7 @@
 package leetcode
 
+/*
+//solution   1
 class LeetCode386 {
     private val comparator = Comparator<Int> { a, b ->
         if (a.toString() < b.toString()) {
@@ -13,5 +15,33 @@ class LeetCode386 {
 
     fun lexicalOrder(n: Int): List<Int> {
         return (1..n).toList().sortedWith(comparator)
+    }
+}
+*/
+
+// solution 2
+class LeetCode386 {
+
+    private fun solve(c: Int, n: Int, ans: MutableList<Int>) {
+        if (c > n) {
+            return
+        }
+
+        if (c != 0) {
+            ans.add(c)
+        }
+
+        for (i in 0..9) {
+            if (c == 0 && i == 0) {
+                continue
+            }
+            solve(c * 10 + i, n, ans)
+        }
+    }
+
+    fun lexicalOrder(n: Int): List<Int> {
+        val ans = mutableListOf<Int>()
+        solve(0, n, ans)
+        return ans
     }
 }
