068523dd6147052475525999e90b693f18dd4e57
Nazmul Islam <nazmulcuet11@gmail.com>
2024-09-23 11:54:48 +0700

    add LeetCode2707 alternate solution using trie

--- acm-kotlin/src/main/kotlin/leetcode/LeetCode2707.kt
+++ acm-kotlin/src/main/kotlin/leetcode/LeetCode2707.kt
@@ -3,9 +3,28 @@ package leetcode
 import kotlin.math.min
 
 class LeetCode2707 {
+    data class TrieNode(
+        var end: Boolean = false,
+        var children: MutableMap<Char, TrieNode> = mutableMapOf<Char, TrieNode>()
+    )
+
     private var s = ""
-    private var words = setOf<String>()
     private var cache = mutableMapOf<Int, Int>()
+    private var trieRoot = TrieNode()
+
+    private fun insertIntoTrie(word: String) {
+        var currentNode = trieRoot
+        for (c in word) {
+            currentNode.children[c]?.let {
+                currentNode = it
+            } ?: run {
+                val node = TrieNode()
+                currentNode.children[c] = node
+                currentNode = node
+            }
+        }
+        currentNode.end = true
+    }
 
     private fun solve(i: Int): Int {
         if (i == s.length) {
@@ -17,10 +36,16 @@ class LeetCode2707 {
         }
 
         var result = 1 + solve(i + 1) // skip
+        var current = trieRoot
         for (j in i until s.length) {
-            val word = s.substring(i, j + 1)
-            if (words.contains(word)) {
-                result = min(result, solve(j + 1))
+            val child = current.children[s[j]]
+            if (child == null) {
+                break
+            } else {
+                current = child
+                if (child.end) {
+                    result = min(result, solve(j + 1))
+                }
             }
         }
         cache[i] = result
@@ -29,7 +54,10 @@ class LeetCode2707 {
 
     fun minExtraChar(s: String, dictionary: Array<String>): Int {
         this.s = s
-        words = dictionary.toSet()
+        this.trieRoot = TrieNode()
+        for (word in dictionary) {
+            insertIntoTrie(word)
+        }
         cache = mutableMapOf<Int, Int>()
         return solve(0)
     }
