447b86b8a3a66fdbf843570a6856af70e3782b82
nazmulcuet11 <nazmulcuet11@gmail.com>
2024-11-14 07:41:56 +0700

    add LeetCode2563 alternate solution

--- acm-kotlin/src/main/kotlin/leetcode/LeetCode2563.kt
+++ acm-kotlin/src/main/kotlin/leetcode/LeetCode2563.kt
@@ -1,5 +1,6 @@
 package leetcode
 
+/*
 class LeetCode2563 {
     private var numbers = listOf<Int>()
 
@@ -41,6 +42,34 @@ class LeetCode2563 {
                 numbers[i] + numbers[y] in lower..upper) {
                 ans += (y - x + 1)
             }
+        }
+        return ans
+    }
+}
+*/
+
+class LeetCode2563 {
+    private fun lowerBound(nums: IntArray, n: Int, s: Int): Int {
+        var l = s
+        var h = nums.size - 1
+        while (l <= h) {
+            val m = (l + h) / 2
+            if (nums[m] < n) {
+                l = m + 1
+            } else {
+                h = m - 1
+            }
+        }
+        return l
+    }
+
+    fun countFairPairs(nums: IntArray, lower: Int, upper: Int): Long {
+        nums.sort()
+        var ans = 0L
+        for (i in 0..<nums.size - 1) {
+            val x = lowerBound(nums ,lower - nums[i], i + 1)
+            val y = lowerBound(nums, upper - nums[i] + 1, i + 1)
+            ans += (y - x)
         }
         return ans
     }
