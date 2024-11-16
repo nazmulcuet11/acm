2a206a4cd2d26de60452c11329861b6896dc26c8
nazmulcuet11 <nazmulcuet11@gmail.com>
2024-11-16 08:12:18 +0700

    add LeetCode3254 with dqueue

--- acm-kotlin/src/main/kotlin/leetcode/LeetCode3254.kt
+++ acm-kotlin/src/main/kotlin/leetcode/LeetCode3254.kt
@@ -3,30 +3,22 @@ package leetcode
 class LeetCode3254 {
     fun resultsArray(nums: IntArray, k: Int): IntArray {
         val ans = mutableListOf<Int>()
-        var r = k - 1
-        var l = k - 1
-        while (l > 0 && nums[l - 1] + 1 == nums[l])
-            l--
-        println("l: $l")
-        if (r - l + 1 == k) {
-            ans.add(nums[r])
-        } else {
-            ans.add(-1)
-        }
-
-        for (i in k..<nums.size) {
-            if (nums[i] != nums[r] + 1) {
-                l = i
+        val dq = ArrayDeque<Int>()
+        for ((i, n) in nums.withIndex()) {
+            if (dq.size > 0 && dq.last() + 1 != n) {
+                dq.clear()
             }
-            r = i
-            if (r - l + 1 > k) {
-                l++
+            dq.add(n)
+            if (dq.size > k) {
+                dq.removeFirst()
             }
 
-            if (r - l + 1 == k) {
-                ans.add(nums[i])
-            } else {
-                ans.add(-1)
+            if (i >= k - 1) {
+                if (dq.size == k) {
+                    ans.add(n)
+                } else {
+                    ans.add(-1)
+                }
             }
         }
         return ans.toIntArray()
