e8a96d2a0a67ee562d5430b165bb96a50c9df01d
nazmulcuet11 <nazmulcuet11@gmail.com>
2024-11-17 17:20:54 +0700

    add LeetCode862 alternate solution

--- acm-kotlin/src/main/kotlin/leetcode/LeetCode862.kt
+++ acm-kotlin/src/main/kotlin/leetcode/LeetCode862.kt
@@ -1,8 +1,8 @@
 package leetcode
 
-import java.util.*
 import kotlin.math.min
 
+/*
 class LeetCode862 {
     data class Info(
         val sum: Long,
@@ -30,6 +30,33 @@ class LeetCode862 {
                 ans = min(ans, i - heap.poll().index)
             }
             heap.add(Info(sum, i))
+        }
+        return if (ans == Int.MAX_VALUE) -1 else ans
+    }
+}
+*/
+
+class LeetCode862 {
+    data class Info(
+        val sum: Long,
+        val index: Int
+    )
+
+    fun shortestSubarray(nums: IntArray, k: Int): Int {
+        val deque = ArrayDeque<Info>()
+        deque.add(Info(0, -1))
+        var sum = 0L
+        var ans = Int.MAX_VALUE
+        for ((i, n) in nums.withIndex()) {
+            sum += n
+            while (deque.isNotEmpty() && sum - deque.first().sum >= k) {
+                ans = min(ans, i - deque.first().index)
+                deque.removeFirst()
+            }
+            while (deque.isNotEmpty() && deque.last().sum >= sum) {
+                deque.removeLast()
+            }
+            deque.add(Info(sum, i))
         }
         return if (ans == Int.MAX_VALUE) -1 else ans
     }
