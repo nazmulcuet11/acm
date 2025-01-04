5646ffd14fbd95db0e6d50b22236587307c8b210
nazmulcuet11 <nazmulcuet11@gmail.com>
2025-01-04 12:04:39 +0700

    add LeetCode1930 alternate solution

--- acm-kotlin/src/main/kotlin/leetcode/LeetCode1930.kt
+++ acm-kotlin/src/main/kotlin/leetcode/LeetCode1930.kt
@@ -1,6 +1,7 @@
 package leetcode
 
 class LeetCode1930 {
+    /*
     fun countPalindromicSubsequence(s: String): Int {
         val charToMinIndex = mutableMapOf<Char, Int>()
         val charToMaxIndex = mutableMapOf<Char, Int>()
@@ -23,5 +24,30 @@ class LeetCode1930 {
             }
         }
         return palindromes.size
+    }
+    */
+
+    // alternate solution
+    fun countPalindromicSubsequence(s: String): Int {
+        val charToMinIndex = mutableMapOf<Char, Int>()
+        val charToMaxIndex = mutableMapOf<Char, Int>()
+        for ((i, c) in s.withIndex()) {
+            if (charToMinIndex[c] == null) {
+                charToMinIndex[c] = i
+            }
+            charToMaxIndex[c] = i
+        }
+
+        var ans = 0
+        for (x in 'a'..'z') {
+            val minIndex = charToMinIndex.getOrDefault(x, s.length)
+            val maxIndex = charToMaxIndex.getOrDefault(x, -1)
+            val uniqueChars = mutableSetOf<Char>()
+            for (i in minIndex + 1..<maxIndex) {
+                uniqueChars.add(s[i])
+            }
+            ans += uniqueChars.count()
+        }
+        return ans
     }
 }
