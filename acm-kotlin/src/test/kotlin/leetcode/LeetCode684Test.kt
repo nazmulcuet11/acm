dd68268bf93207b3bab5b2d0549f2755ef1e7f84
nazmulcuet11 <nazmulcuet11@gmail.com>
2025-01-29 09:32:16 +0700

    add LeetCode684 testcase

--- acm-kotlin/src/test/kotlin/leetcode/LeetCode684Test.kt
+++ acm-kotlin/src/test/kotlin/leetcode/LeetCode684Test.kt
@@ -12,11 +12,27 @@ class LeetCode684Test {
             intArrayOf(1, 4),
             sut.findRedundantConnection(
                 arrayOf(
-                    intArrayOf(1,2),
-                    intArrayOf(2,3),
-                    intArrayOf(3,4),
-                    intArrayOf(1,4),
-                    intArrayOf(1,5),
+                    intArrayOf(1, 2),
+                    intArrayOf(2, 3),
+                    intArrayOf(3, 4),
+                    intArrayOf(1, 4),
+                    intArrayOf(1, 5),
+                )
+            )
+        )
+    }
+
+    @Test
+    fun case2() {
+        assertContentEquals(
+            intArrayOf(4, 5),
+            sut.findRedundantConnection(
+                arrayOf(
+                    intArrayOf(2, 4),
+                    intArrayOf(3, 4),
+                    intArrayOf(1, 4),
+                    intArrayOf(2, 5),
+                    intArrayOf(4, 5),
                 )
             )
         )
