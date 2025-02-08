9444a3e8cc9728b032d0ab9a3c40cfe0efa643fb
nazmulcuet11 <nazmulcuet11@gmail.com>
2025-02-08 07:45:31 +0700

    add LeetCode2349

---
+++ acm-kotlin/src/main/kotlin/leetcode/Leet2349.kt
@@ -0,0 +1,31 @@
+package leetcode
+
+import java.util.*
+
+class Leet2349 {
+    class NumberContainers() {
+        private val indexToNumber = mutableMapOf<Int, Int>()
+        private val numberToIndices = mutableMapOf<Int, PriorityQueue<Int>>()
+
+        fun change(index: Int, number: Int) {
+            indexToNumber[index] = number
+            if (numberToIndices[number] == null) {
+                numberToIndices[number] = PriorityQueue()
+            }
+            numberToIndices[number]?.add(index)
+        }
+
+        fun find(number: Int): Int {
+            val queue = numberToIndices[number] ?: return -1
+            while (queue.isNotEmpty()) {
+                val index = queue.peek()
+                if (indexToNumber[index] == number) {
+                    return index
+                } else {
+                    queue.poll()
+                }
+            }
+            return -1
+        }
+    }
+}
---
+++ acm-kotlin/src/test/kotlin/leetcode/Leet2349Test.kt
@@ -0,0 +1,20 @@
+package leetcode
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class Leet2349Test {
+    @Test
+    fun case1() {
+        val sut = Leet2349.NumberContainers()
+
+        assertEquals(-1, sut.find(10))
+        sut.change(2, 10)
+        sut.change(1, 10)
+        sut.change(3, 10)
+        sut.change(5, 10)
+        assertEquals(1, sut.find(10))
+        sut.change(1, 20)
+        assertEquals(2, sut.find(10))
+    }
+}
