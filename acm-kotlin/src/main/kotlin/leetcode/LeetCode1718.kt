39959c312cd473269c9e78a235b0fbb17c8179a7
nazmulcuet11 <nazmulcuet11@gmail.com>
2025-02-16 20:29:58 +0700

    refactor LeetCode1718

--- acm-kotlin/src/main/kotlin/leetcode/LeetCode1718.kt
+++ acm-kotlin/src/main/kotlin/leetcode/LeetCode1718.kt
@@ -1,38 +1,18 @@
 package leetcode
 
 class LeetCode1718 {
-    private var ans = IntArray(0)
-
-    private fun compareAndUpdateAns(current: IntArray) {
-        for (i in current.indices) {
-            if (current[i] < ans[i]) {
-                return
-            } else if (current[i] > ans[i]) {
-                ans = current.copyOf()
-                return
-            }
-        }
-    }
-
     private fun solve(
         i: Int,
         n: Int,
-        current: IntArray,
-        taken: BooleanArray,
-        remaining: Int
+        ans: IntArray,
+        taken: BooleanArray
     ): Boolean {
-        if (remaining == 0) {
-            compareAndUpdateAns(current)
+        if (i == ans.size) {
             return true
         }
 
-        if (i == current.size) {
-            // invalid
-            return false
-        }
-
-        if (current[i] != 0) {
-            return solve(i + 1, n, current, taken, remaining)
+        if (ans[i] != 0) {
+            return solve(i + 1, n, ans, taken)
         }
 
         for (j in n downTo 1) {
@@ -40,23 +20,19 @@ class LeetCode1718 {
                 continue
             }
 
-            if (j == 1) {
+            if (j == 1 || (i + j < ans.size && ans[i + j] == 0)) {
                 taken[j] = true
-                current[i] = j
-                if (solve(i + 1, n, current, taken, remaining - 1)) {
+                ans[i] = j
+                if (j > 1) {
+                    ans[i + j] = j
+                }
+                if (solve(i + 1, n, ans, taken)) {
                     return true
                 }
-                current[i] = 0
-                taken[j] = false
-            } else if (i + j < current.size && current[i + j] == 0) {
-                taken[j] = true
-                current[i] = j
-                current[i + j] = j
-                if (solve(i + 1, n, current, taken, remaining - 1)) {
-                    return true
+                ans[i] = 0
+                if (j > 1) {
+                    ans[i + j] = 0
                 }
-                current[i] = 0
-                current[i + j] = 0
                 taken[j] = false
             }
         }
@@ -65,10 +41,9 @@ class LeetCode1718 {
 
     fun constructDistancedSequence(n: Int): IntArray {
         val ansSize = n * 2 - 1
-        ans = IntArray(ansSize) { 0 }
-        val current = IntArray(ansSize) { 0 }
+        val ans = IntArray(ansSize) { 0 }
         val taken = BooleanArray(n + 1) { false }
-        solve(0, n, current, taken, n)
+        solve(0, n, ans, taken)
         return ans
     }
 }
