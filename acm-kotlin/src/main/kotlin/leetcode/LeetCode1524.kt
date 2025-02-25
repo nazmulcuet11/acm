fc86e9da55e4faa3eb2592da475069a31b6530b1
nazmulcuet11 <nazmulcuet11@gmail.com>
2025-02-25 08:04:21 +0700

    refactor LeetCode1524

--- acm-kotlin/src/main/kotlin/leetcode/LeetCode1524.kt
+++ acm-kotlin/src/main/kotlin/leetcode/LeetCode1524.kt
@@ -2,7 +2,7 @@ package leetcode
 
 class LeetCode1524 {
     companion object {
-        const val MOD = 1000_000_000 + 7
+        const val MOD = 1_000_000_000 + 7
     }
 
     fun numOfSubarrays(arr: IntArray): Int {
@@ -10,29 +10,21 @@ class LeetCode1524 {
             arr[i] += arr[i - 1]
         }
 
-        val oddCount = IntArray(arr.size) { 0 }
-        val evenCount = IntArray(arr.size) { 0 }
+        var oddCount = 0
+        var evenCount = 0
+        var ans = 0
         for (i in arr.size - 1 downTo 0) {
             if (arr[i] % 2 == 0) {
-                evenCount[i]++
+                evenCount++
             } else {
-                oddCount[i]++
+                oddCount++
             }
 
-            if (i < arr.size - 1) {
-                evenCount[i] += evenCount[i + 1]
-                oddCount[i] += oddCount[i + 1]
-            }
-        }
-
-        var ans = 0
-        for (i in arr.indices) {
-            ans += if (i == 0 || arr[i - 1] % 2 == 0) {
-                oddCount[i] % MOD
+            ans = if (i == 0 || arr[i - 1] % 2 == 0) {
+                (ans + oddCount) % MOD
             } else {
-                evenCount[i] % MOD
+                (ans + evenCount) % MOD
             }
-            ans %= MOD
         }
         return ans
     }
