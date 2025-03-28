1a3f367be4384d694118b396407f2af9d8f60b0d
nazmulcuet11 <nazmulcuet11@gmail.com>
2025-03-28 17:22:05 +0700

    refactor LeetCode2503

--- acm-kotlin/src/main/kotlin/leetcode/LeetCode2503.kt
+++ acm-kotlin/src/main/kotlin/leetcode/LeetCode2503.kt
@@ -4,60 +4,38 @@ import java.util.*
 
 class LeetCode2503 {
     fun maxPoints(grid: Array<IntArray>, queries: IntArray): IntArray {
-        val sortedQueries = queries.withIndex().sortedBy { it.value }
+        data class QueueEntry(val v: Int, val i: Int, val j: Int)
 
-        data class GridIndex(val i: Int, val j: Int)
-        data class QueueEntry(val v: Int, val g: GridIndex)
+        fun isValid(i: Int, j: Int) = i >= 0 && i < grid.size && j >= 0 && j < grid[i].size
 
-        fun isValid(g: GridIndex): Boolean {
-            return g.i >= 0 && g.i < grid.size && g.j >= 0 && g.j < grid.first().size
-        }
-
-        fun getValidNeighbors(g: GridIndex): List<GridIndex> {
-            val offset = listOf(
-                GridIndex(0, 1),
-                GridIndex(1, 0),
-                GridIndex(-1, 0),
-                GridIndex(0, -1)
-            )
-
-            val validNeighbors = mutableListOf<GridIndex>()
-            for (o in offset) {
-                val neighbor = GridIndex(g.i + o.i, g.j + o.j)
-                if (isValid(neighbor)) {
-                    validNeighbors.add(neighbor)
-                }
-            }
-            return validNeighbors
-        }
-
-        fun isVisited(g: GridIndex): Boolean {
-            return grid[g.i][g.j] == -1
-        }
+        fun isVisited(i: Int, j: Int) = grid[i][j] == -1
 
         val comparator = Comparator<QueueEntry> { a, b -> a.v - b.v }
 
         var count = 0
         val pq = PriorityQueue(comparator)
-        fun dijkstra(x: Int) {
-            while (pq.isNotEmpty() && pq.peek().v < x) {
-                val (_, g) = pq.poll()
-                if (isVisited(g)) {
+        fun dijkstra(n: Int) {
+            while (pq.isNotEmpty() && pq.peek().v < n) {
+                val (_, i, j) = pq.poll()
+                if (isVisited(i, j)) {
                     continue
                 }
-                grid[g.i][g.j] = -1
+
+                grid[i][j] = -1
                 count++
-                val neighbors = getValidNeighbors(g)
-                for (n in neighbors) {
-                    if (!isVisited(n)) {
-                        pq.add(QueueEntry(grid[n.i][n.j], n))
+                val dx = arrayOf(0, 0, 1, -1)
+                val dy = arrayOf(1, -1, 0, 0)
+                for ((x, y) in dx.zip(dy)) {
+                    if (isValid(i + x, j + y)) {
+                        pq.add(QueueEntry(grid[i + x][j + y], i + x, j + y))
                     }
                 }
             }
         }
 
+        val sortedQueries = queries.withIndex().sortedBy { it.value }
         val ans = IntArray(sortedQueries.size)
-        pq.add(QueueEntry(grid[0][0], GridIndex(0, 0)))
+        pq.add(QueueEntry(grid[0][0], 0, 0))
         for ((i, q) in sortedQueries) {
             dijkstra(q)
             ans[i] = count
