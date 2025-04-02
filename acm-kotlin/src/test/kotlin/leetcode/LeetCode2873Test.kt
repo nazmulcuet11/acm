9f4ed5ac0b599dc084bab9a7200da9ac687aa25e
nazmulcuet11 <nazmulcuet11@gmail.com>
2025-04-02 08:09:43 +0700

    add LeetCode2873

---
+++ acm-kotlin/src/test/kotlin/leetcode/LeetCode2873Test.kt
@@ -0,0 +1,32 @@
+package leetcode
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class LeetCode2873Test {
+    private val sut = LeetCode2873()
+
+    @Test
+    fun case1() {
+        assertEquals(
+            77,
+            sut.maximumTripletValue(intArrayOf(12, 6, 1, 2, 7))
+        )
+    }
+
+    @Test
+    fun case2() {
+        assertEquals(
+            133,
+            sut.maximumTripletValue(intArrayOf(1, 10, 3, 4, 19))
+        )
+    }
+
+    @Test
+    fun case3() {
+        assertEquals(
+            0,
+            sut.maximumTripletValue(intArrayOf(1, 2, 3))
+        )
+    }
+}
