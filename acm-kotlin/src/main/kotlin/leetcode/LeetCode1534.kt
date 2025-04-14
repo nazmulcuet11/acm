23bd529d7a5a3ad7f399a8151fc09b8c9e30ea56
nazmulcuet11 <nazmulcuet11@gmail.com>
2025-04-14 10:50:39 +0700

    refactor LeetCode1534

--- acm-kotlin/src/main/kotlin/leetcode/LeetCode1534.kt
+++ acm-kotlin/src/main/kotlin/leetcode/LeetCode1534.kt
@@ -5,7 +5,7 @@ import kotlin.math.abs
 class LeetCode1534 {
     fun countGoodTriplets(arr: IntArray, a: Int, b: Int, c: Int): Int {
         var count = 0
-        for(i in 0..<arr.size) {
+        for (i in 0..<arr.size) {
             for (j in i + 1..<arr.size) {
                 for (k in j + 1..<arr.size) {
                     if (abs(arr[i] - arr[j]) <= a && abs(arr[j] - arr[k]) <= b && abs(arr[i] - arr[k]) <= c) {
