9b194ddb49122da1cf9a162bd58d443dbc239dd5
nazmulcuet11 <nazmulcuet11@gmail.com>
2025-06-09 20:20:35 +0700

    refactor LeetCode440

--- acm-kotlin/src/main/kotlin/leetcode/LeetCode440.kt
+++ acm-kotlin/src/main/kotlin/leetcode/LeetCode440.kt
@@ -3,28 +3,28 @@ package leetcode
 import kotlin.math.min
 
 class LeetCode440 {
-    private fun countNodes(curr: Long, next: Long, n: Long): Long {
-        if (curr > n) {
-            return 0
-        }
-
-        return (min(next, n + 1) - curr) + countNodes(curr * 10, next * 10, n)
-    }
-
-    private fun solve(c: Int, n: Int, k: Int): Int {
-        if (k == 1) {
-            return c
-        }
-
-        val count = countNodes(c.toLong(), c + 1L, n.toLong()).toInt()
-        return if (count >= k) {
-            solve(c * 10, n, k - 1)
-        } else {
-            solve(c + 1, n, k - count)
-        }
-    }
-
     fun findKthNumber(n: Int, k: Int): Int {
-        return solve(1, n, k)
+        fun countNodes(curr: Long, next: Long): Long {
+            if (curr > n) {
+                return 0
+            }
+
+            return (min(next, n + 1L) - curr) + countNodes(curr * 10, next * 10)
+        }
+
+        fun solve(c: Int, k: Int): Int {
+            if (k == 1) {
+                return c
+            }
+
+            val count = countNodes(c.toLong(), c + 1L).toInt()
+            return if (count >= k) {
+                solve(c * 10, k - 1)
+            } else {
+                solve(c + 1, k - count)
+            }
+        }
+
+        return solve(1, k)
     }
 }
