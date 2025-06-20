e83f22376177f7e678ac01583beb9c97781306d5
nazmulcuet11 <nazmulcuet11@gmail.com>
2025-06-20 16:36:34 +0700

    add LeetCode3443

--- acm-kotlin/src/main/kotlin/leetcode/LeetCode3443.kt
+++ acm-kotlin/src/main/kotlin/leetcode/LeetCode3443.kt
@@ -1,49 +1,18 @@
 package leetcode
 
-import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.min
 
 class LeetCode3443 {
     fun maxDistance(s: String, k: Int): Int {
-        fun solve(n: Int, s: Int, e: Int, w: Int): Int {
-            var nCount = n
-            var sCount = s
-            var eCount = e
-            var wCount = w
-
-            var remainingAlter = k
-            if (nCount > sCount) {
-                val numberOfAlter = min(sCount, remainingAlter)
-                nCount += numberOfAlter
-                sCount -= numberOfAlter
-                remainingAlter -= numberOfAlter
-            } else {
-                val numberOfAlter = min(nCount, remainingAlter)
-                sCount += numberOfAlter
-                nCount -= numberOfAlter
-                remainingAlter -= numberOfAlter
-            }
-
-            if (wCount > eCount) {
-                val numberOfAlter = min(eCount, remainingAlter)
-                wCount += numberOfAlter
-                eCount -= numberOfAlter
-                remainingAlter -= numberOfAlter
-            } else {
-                val numberOfAlter = min(wCount, remainingAlter)
-                eCount += numberOfAlter
-                wCount -= numberOfAlter
-                remainingAlter -= numberOfAlter
-            }
-
-            // println(nCount)
-            // println(sCount)
-            // println(wCount)
-            // println(eCount)
-
-            return abs(nCount - sCount) + abs(wCount - eCount)
+        fun solve(a: Int, b: Int, k: Int): Int {
+            val x = min(a, b)
+            val y = max(a, b)
+            val z = minOf(a, b, k)
+            return x - y + 2 * z
         }
+
+        fun solve(n: Int, s: Int, e: Int, w: Int) = solve(n, s, k) + solve(e, w, k - minOf(n, s, k))
 
         var nCount = 0
         var sCount = 0
