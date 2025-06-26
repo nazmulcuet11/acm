9e661fcc42ae5c060835020f4698efea8a456f8a
Nazmul Islam <nazmulcuet11@gmail.com>
2025-06-26 12:40:18 +0700

    add LeetCode2311

--- acm-kotlin/src/main/kotlin/leetcode/LeetCode2311.kt
+++ acm-kotlin/src/main/kotlin/leetcode/LeetCode2311.kt
@@ -2,6 +2,7 @@ package leetcode
 
 import kotlin.math.min
 
+/*
 class LeetCode2311 {
     fun longestSubsequence(s: String, k: Int): Int {
         val n = s.length
@@ -30,5 +31,63 @@ class LeetCode2311 {
             }
         }
         return left
+    }
+}
+*/
+
+/*
+class LeetCode2311 {
+    fun longestSubsequence(s: String, k: Int): Int {
+        val n = s.length
+        val taken = BooleanArray(n) {  s[it] == '0' }
+
+        fun verify(): Boolean {
+            var number = 0L
+            for (i in 0..<n) {
+                if (taken[i]) {
+                    number = number * 2 + s[i].digitToInt()
+                }
+                if (number > k) {
+                    return false
+                }
+            }
+            return true
+        }
+
+        var ans = taken.count { it }
+        for (i in n - 1 downTo 0) {
+            if (s[i] == '1') {
+                taken[i] = true
+                if (verify()) {
+                    ans++
+                } else {
+                    break
+                }
+            }
+        }
+
+        return ans
+    }
+}
+*/
+
+class LeetCode2311 {
+    fun longestSubsequence(s: String, k: Int): Int {
+        val n = s.length
+        val zeroCount = s.count { it == '0' }
+        var mask = 0L
+        var ans = zeroCount
+        for (i in 0..<min(n, 64)) {
+            if (s[n - 1 - i] == '1') {
+                mask = mask or (1L shl i)
+                if (mask <= k) {
+                    ans++
+                } else {
+                    break
+                }
+            }
+        }
+
+        return ans
     }
 }
