c0cee3d9867513c8fd0ad670277ff0874c38bc5a
Nazmul Islam <nazmulcuet11@gmail.com>
2025-07-09 09:39:03 +0700

    add LeetCode3439

---
+++ acm-kotlin/src/main/kotlin/leetcode/LeetCode3439.kt
@@ -0,0 +1,53 @@
+package leetcode
+
+import kotlin.math.max
+import kotlin.math.min
+
+class LeetCode3439 {
+    fun maxFreeTime(eventTime: Int, k: Int, startTime: IntArray, endTime: IntArray): Int {
+        data class Meeting(val start: Int, val end: Int)
+
+        val meetings = mutableListOf<Meeting>()
+        startTime.zip(endTime).forEach {
+            meetings.add(Meeting(it.first, it.second))
+        }
+
+        meetings.sortBy { it.start }
+
+        val gaps = mutableListOf<Int>()
+        var lastEnd = 0
+        for (meeting in meetings) {
+            val gap = meeting.start - lastEnd
+            if (gap > 0) {
+                gaps.add(gap)
+            }
+            lastEnd = meeting.end
+        }
+
+        if (lastEnd < eventTime) {
+            gaps.add(eventTime - lastEnd)
+        }
+
+        var sum = 0
+        for (i in 0..<min(k + 1, gaps.size)) {
+            sum += gaps[i]
+        }
+
+        var ans = sum
+        for (i in k + 1..<gaps.size) {
+            sum += gaps[i]
+            sum -= gaps[i - k - 1]
+            ans = max(ans, sum)
+        }
+
+        return ans
+    }
+}
+
+/*
+
+k = 2
+__*_*_*_*_*_*_*_
+
+
+*/
