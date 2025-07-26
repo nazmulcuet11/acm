8cec4fa47640df46de2e2b6c16aba827e0b1fbd4
nazmulcuet11 <nazmulcuet11@gmail.com>
2025-07-27 06:34:37 +0700

    add LeetCode3480

---
+++ acm-kotlin/src/main/kotlin/leetcode/LeetCode3480.kt
@@ -0,0 +1,43 @@
+package leetcode
+
+import kotlin.math.max
+
+class LeetCode3480 {
+    fun maxSubarrays(n: Int, conflictingPairs: Array<IntArray>): Long {
+        val maxLefts = List(n + 1) { mutableListOf(0, 0) }
+        for (pair in conflictingPairs) {
+            pair.sort()
+            val l = pair[0]
+            val r = pair[1]
+            val lefts = maxLefts[r]
+            if (l > lefts[1]) {
+                lefts[0] = lefts[1]
+                lefts[1] = l
+            } else if (l > lefts[0]) {
+                lefts[0] = l
+            }
+        }
+
+        val gain = MutableList(n + 1) { 0L }
+        var maxGain = 0L
+        var ansWithoutRemoving = 0L
+        var left = 0
+        var secondLeft = 0
+        for (r in 1..n) {
+            for (l in maxLefts[r]) {
+                if (l > left) {
+                    secondLeft = left
+                    left = l
+                } else if (l > secondLeft) {
+                    secondLeft = l
+                }
+            }
+
+            ansWithoutRemoving += r - left
+            gain[left] += left - secondLeft
+            maxGain = max(maxGain, gain[left])
+        }
+
+        return ansWithoutRemoving + maxGain
+    }
+}
